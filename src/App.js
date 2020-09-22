11ea01b743146111d77906aef542b8e28e4c95d1
Jerome Drevet <jerome.drevet@protonmail.com>
2020-09-22 17:34:39 +0200

    create a calculator

--- src/App.js
+++ src/App.js
@@ -2,22 +2,56 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+let total = 0
+let inputArray = []
+let someMaths = {
+  '+': function (x, y) { return x + y },
+  '-': function (x, y) { return x - y },
+  '*': function (x, y) { return x * y },
+  '/': function (x, y) { return x / y }
+}
+
 function App() {
+  function handleTotal() {
+    inputArray.push(parseInt(document.getElementById("calculatorInput").value))
+
+    inputArray.forEach((item, index) => {
+      if(typeof item === "number") {
+        return
+      }
+
+      total = someMaths[inputArray[index]](inputArray[index - 1], inputArray[index + 1])
+    })
+
+    document.getElementById("calculatorInput").value = total
+    total = 0
+  }
+
+  function handleReset() {
+    total = 0
+    document.getElementById("calculatorInput").value = ""
+  }
+
+  function storeValue(event) {
+    inputArray.push(parseInt(document.getElementById("calculatorInput").value))
+    inputArray.push(event.target.value)
+    document.getElementById("calculatorInput").value = ""
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <input id="calculatorInput"/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <button onClick={storeValue} value="+">+</button>
+          <button onClick={storeValue} value="-">-</button>
+          <button onClick={storeValue} value="*">x</button>
+          <button onClick={storeValue} value="/">/</button>
+          <button onClick={handleReset}>Reset</button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          <button onClick={handleTotal}>=</button>
+        </p>
       </header>
     </div>
   );
